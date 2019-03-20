50dafb6e77ecb3b82fb4bd2c9a819406b390c73e
MatMark <37310556+MatMark@users.noreply.github.com>
2019-03-20 21:25:56 +0100

    Class for tests

---
+++ Lab 2 - ClassLoader/MyJComponents/ButtonClass.java
@@ -0,0 +1,41 @@
+package MyJComponents;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonClass extends JButton implements ActionListener {
+
+    private int h, w;
+    public ButtonClass() {
+        super("Don't touch");
+        super.setPreferredSize(new Dimension(100,50));
+        h = (int) super.getPreferredSize().getHeight();
+        w = (int) super.getPreferredSize().getWidth();
+        addActionListener(this);
+    }
+
+
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        g.setColor(Color.red);
+        g.fillOval(5, (h/2)-5, 10, 10);
+        g.fillOval(w-15, (h/2)-5, 10, 10);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource().equals(this))
+        {
+            JOptionPane.showMessageDialog(null, "You touch it!", "DON'T TOUCH!!!", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ButtonClass";
+    }
+}
---
+++ Lab 2 - ClassLoader/MyJComponents/TextBoxClass.java
@@ -0,0 +1,30 @@
+package MyJComponents;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TextBoxClass extends JTextField{
+    private int h, w;
+
+    public TextBoxClass() {
+        super("Notatka");
+        super.setPreferredSize(new Dimension(100, 50));
+        super.setHorizontalAlignment(JTextField.CENTER);
+        h = (int) super.getPreferredSize().getHeight();
+        w = (int) super.getPreferredSize().getWidth();
+    }
+
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        g.setColor(Color.green);
+        g.fillOval(5, (h/2)-5, 10, 10);
+        g.fillOval(w-15, (h/2)-5, 10, 10);
+    }
+
+    @Override
+    public String toString() {
+        return "TextBoxClass";
+    }
+}
